b8d57c9cdb3b52c240790c645ebc27b10facaf7b
Cossta07 <samuellinker46@gmail.com>
2024-04-10 14:57:47 -0400

    questao 4 feita

---
+++ questao4.js
@@ -0,0 +1,8 @@
+/*
+Escreva um código JavaScript utilizando o operador ternário para 
+determinar se um número é positivo, negativo ou zero. A variável numero 
+contém o valor a ser verificado. Atribua o resultado à variável resultado.
+*/
+let valor = 0;
+let resultado = valor > 0 ? "positivo" : (valor < 0 ? "negativo" : "zero");
+console.log("O número é " + resultado + ".");
