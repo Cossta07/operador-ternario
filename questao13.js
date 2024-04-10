a359fc7ba5b9ed98e922c328a68aa916b5834d19
Cossta07 <samuellinker46@gmail.com>
2024-04-10 17:11:49 -0400

    questao 13 feita

--- questao13.js
+++ questao13.js
@@ -4,7 +4,8 @@ determinar se uma pessoa é elegível para a aposentadoria. A variável idade
 contém a idade da pessoa e a variável sexo indica o sexo ( 'masculino' ou 
 'feminino'). Atribua o resultado à variável elegivel.
 */ 
-let idade = 65;
-let sexo = 'masculino';
-let elegivel = (sexo === 'feminino' && idade >= 62) || (sexo === 'masculino' && idade >= 65) ? 'pode aposentaa' : 'nao pode aposentar';
+let idade = 60;
+let sexo = 'masculino'; // ou coloca feminino 
+
+let elegivel = (sexo === 'masculino' && idade >= 65) || (sexo === 'feminino' && idade >= 62) ? "pode aposenta" : "Não pode aposenta";
 console.log(elegivel);
