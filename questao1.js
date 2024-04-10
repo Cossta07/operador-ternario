15137ea0da1858d5f103fd0123005343e4ee9ea8
Cossta07 <samuellinker46@gmail.com>
2024-04-10 14:35:36 -0400

    questao 1 e 2 feita

---
+++ questao1.js
@@ -0,0 +1,9 @@
+/*
+Escreva um código JavaScript utilizando o operador ternário para 
+determinar se um número é par ou ímpar. A variável numero contém o valor a 
+ser verificado. Atribua o resultado à variável resultado.
+*/
+
+let numero = 10 ;
+let status = (numero % 2 == 0 )?"par":"impar";
+console.log(status);
---
+++ questao2.js
@@ -0,0 +1,8 @@
+/*
+Escreva um código JavaScript utilizando o operador ternário para 
+determinar se um aluno foi aprovado ou reprovado em um exame. A variável 
+nota contém a pontuação do aluno. Atribua o resultado à variável status.
+*/
+let nota = 6;
+let status = (nota >= 6 )?"aprovado":"reprovado";
+console.log(status);
